1249790ccdd55dc61b51e8cd64b641ca369161a1
Bruno Tarditi <brunotarditi@hotmail.com>
2021-11-17 21:37:07 -0300

    Primer commit

---
+++ IdeaProjects/WebSocketExample/src/main/java/com/example/websocket/websocketexample/entities/Persona.java
@@ -0,0 +1,20 @@
+package com.example.websocket.websocketexample.entities;
+
+public class Automoviles {
+    private String numero;
+
+    public Automoviles() {
+    }
+
+    public Automoviles(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
