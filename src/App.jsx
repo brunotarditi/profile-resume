8aa39a2b416b15e39b6c66d408bc06c46a89852a
Bruno Tarditi <brunotarditi@hotmail.com>
2021-11-17 22:50:14 -0300

    agregando mui material

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+function App() {
+  return (
+    <div className="App">
+     <Button variant="contained">
+       CV Bruno Tarditi
+     </Button>
+    </div>
+  );
+}
+
+export default App;
